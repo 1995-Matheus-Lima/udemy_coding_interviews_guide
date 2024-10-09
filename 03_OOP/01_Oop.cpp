a90ad2ce58c40f10397e0a85372c24b47e5d3ada
Matheus Henrique <1995.matheus.lima@gmail.com>
2024-10-09 14:25:02 -0300

    started Basic Sort Algorthm studies

---
+++ 03_OOP/01_Oop.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class Product{
+	public:
+		int id;
+		int wieght;
+		char name[50];
+
+		void setName(char name[50]){
+			this->name = name;
+		}
+		
+		void printName(){
+			if(name){
+			cout << name << endl;
+			}
+		}
+};
+
+int main(){
+	Produtable;
+	table.setName("spider man table for kids");
+	table.printName();
+
+}
+
+
